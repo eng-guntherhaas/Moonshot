d32031f4386907aa9e7425c0d4a59648c2ca8a27
guntherhaas <guntherhaas.eng@gmail.com>
2024-06-18 15:27:51 +0200

    initial commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Gunther Masi Haas on 18/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
